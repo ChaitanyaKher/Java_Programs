27a02d9793688a0c8ff4c713eb479bf09d708ccf
CHAITANYA\Chaitanya <chaitanyakher2131996@gmail.com>
2018-06-20 11:52:59 +0530

    initial commit

---
+++ Example2.java
@@ -0,0 +1,12 @@
+class Example2
+{
+public static void main(String args[])
+	{
+		int num;
+		num=100;//assign number 2 to num
+		System.out.println("This is num: ", + num);
+		num=num*2;
+		System.out.print("The value of num*2 is");
+		System.out.println(num);
+	}
+}
