11932eb06a050c7e79f6d17a40928e99883e3b22
CHAITANYA\Chaitanya <chaitanyakher2131996@gmail.com>
2018-08-18 08:33:04 +0530

    18 Aug18

---
+++ bool.java
@@ -0,0 +1,19 @@
+class BoolTest
+{
+	public static void main(String[] args){
+	      boolean b;
+	      b=false;
+int y= 123_456_789;
+	int x=0x0A;
+	System.out.println("x is " +x);
+	      System.out.println("b is " +b);
+	      b=true;
+	      System.out.println("b is " +b);
+	      if(b) 
+	      System.out.println("This is executed.");
+	b = false;
+	if(b)
+	 System.out.println("This is not executed.");
+	System.out.println("10 > 9 is " + (10 > 9));
+	}
+}
